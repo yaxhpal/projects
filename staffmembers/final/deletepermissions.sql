0b9a050d78e3bfd2939ec992fd7ccdd9fe08d297
Yashpal Meena <yaxhpal@gmail.com>
2014-02-09 15:23:13 +0530

    Added new project staffmember

---
+++ staffmembers/final/deletepermissions.sql
@@ -0,0 +1,318 @@
+UPDATE borrowers SET flags='0' WHERE borrowernumber='20321';
+DELETE FROM user_permissions   WHERE borrowernumber = '20321';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='22214';
+DELETE FROM user_permissions   WHERE borrowernumber = '22214';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='29117';
+DELETE FROM user_permissions   WHERE borrowernumber = '29117';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='29118';
+DELETE FROM user_permissions   WHERE borrowernumber = '29118';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='29119';
+DELETE FROM user_permissions   WHERE borrowernumber = '29119';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='29120';
+DELETE FROM user_permissions   WHERE borrowernumber = '29120';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='29123';
+DELETE FROM user_permissions   WHERE borrowernumber = '29123';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='29125';
+DELETE FROM user_permissions   WHERE borrowernumber = '29125';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='29127';
+DELETE FROM user_permissions   WHERE borrowernumber = '29127';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='29129';
+DELETE FROM user_permissions   WHERE borrowernumber = '29129';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='41003';
+DELETE FROM user_permissions   WHERE borrowernumber = '41003';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='41004';
+DELETE FROM user_permissions   WHERE borrowernumber = '41004';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='41005';
+DELETE FROM user_permissions   WHERE borrowernumber = '41005';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='41008';
+DELETE FROM user_permissions   WHERE borrowernumber = '41008';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='49186';
+DELETE FROM user_permissions   WHERE borrowernumber = '49186';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='49187';
+DELETE FROM user_permissions   WHERE borrowernumber = '49187';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='52127';
+DELETE FROM user_permissions   WHERE borrowernumber = '52127';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='53241';
+DELETE FROM user_permissions   WHERE borrowernumber = '53241';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='53391';
+DELETE FROM user_permissions   WHERE borrowernumber = '53391';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='64816';
+DELETE FROM user_permissions   WHERE borrowernumber = '64816';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='71132';
+DELETE FROM user_permissions   WHERE borrowernumber = '71132';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='81584';
+DELETE FROM user_permissions   WHERE borrowernumber = '81584';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='82641';
+DELETE FROM user_permissions   WHERE borrowernumber = '82641';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='92181';
+DELETE FROM user_permissions   WHERE borrowernumber = '92181';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='106431';
+DELETE FROM user_permissions   WHERE borrowernumber = '106431';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='106444';
+DELETE FROM user_permissions   WHERE borrowernumber = '106444';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='112731';
+DELETE FROM user_permissions   WHERE borrowernumber = '112731';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='112733';
+DELETE FROM user_permissions   WHERE borrowernumber = '112733';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='112737';
+DELETE FROM user_permissions   WHERE borrowernumber = '112737';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='112742';
+DELETE FROM user_permissions   WHERE borrowernumber = '112742';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='112743';
+DELETE FROM user_permissions   WHERE borrowernumber = '112743';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='112745';
+DELETE FROM user_permissions   WHERE borrowernumber = '112745';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='118716';
+DELETE FROM user_permissions   WHERE borrowernumber = '118716';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='118864';
+DELETE FROM user_permissions   WHERE borrowernumber = '118864';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='122964';
+DELETE FROM user_permissions   WHERE borrowernumber = '122964';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='132708';
+DELETE FROM user_permissions   WHERE borrowernumber = '132708';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='140085';
+DELETE FROM user_permissions   WHERE borrowernumber = '140085';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='140088';
+DELETE FROM user_permissions   WHERE borrowernumber = '140088';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='144202';
+DELETE FROM user_permissions   WHERE borrowernumber = '144202';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='144261';
+DELETE FROM user_permissions   WHERE borrowernumber = '144261';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='144281';
+DELETE FROM user_permissions   WHERE borrowernumber = '144281';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='144291';
+DELETE FROM user_permissions   WHERE borrowernumber = '144291';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='144297';
+DELETE FROM user_permissions   WHERE borrowernumber = '144297';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='144308';
+DELETE FROM user_permissions   WHERE borrowernumber = '144308';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='164018';
+DELETE FROM user_permissions   WHERE borrowernumber = '164018';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='167932';
+DELETE FROM user_permissions   WHERE borrowernumber = '167932';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='168032';
+DELETE FROM user_permissions   WHERE borrowernumber = '168032';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='168480';
+DELETE FROM user_permissions   WHERE borrowernumber = '168480';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='168665';
+DELETE FROM user_permissions   WHERE borrowernumber = '168665';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='168667';
+DELETE FROM user_permissions   WHERE borrowernumber = '168667';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='169407';
+DELETE FROM user_permissions   WHERE borrowernumber = '169407';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='169410';
+DELETE FROM user_permissions   WHERE borrowernumber = '169410';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='169615';
+DELETE FROM user_permissions   WHERE borrowernumber = '169615';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='169619';
+DELETE FROM user_permissions   WHERE borrowernumber = '169619';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='169621';
+DELETE FROM user_permissions   WHERE borrowernumber = '169621';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='170347';
+DELETE FROM user_permissions   WHERE borrowernumber = '170347';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='171588';
+DELETE FROM user_permissions   WHERE borrowernumber = '171588';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='172099';
+DELETE FROM user_permissions   WHERE borrowernumber = '172099';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='172115';
+DELETE FROM user_permissions   WHERE borrowernumber = '172115';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='187550';
+DELETE FROM user_permissions   WHERE borrowernumber = '187550';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='187586';
+DELETE FROM user_permissions   WHERE borrowernumber = '187586';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='187607';
+DELETE FROM user_permissions   WHERE borrowernumber = '187607';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='187614';
+DELETE FROM user_permissions   WHERE borrowernumber = '187614';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='187615';
+DELETE FROM user_permissions   WHERE borrowernumber = '187615';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='187623';
+DELETE FROM user_permissions   WHERE borrowernumber = '187623';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='187635';
+DELETE FROM user_permissions   WHERE borrowernumber = '187635';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='187655';
+DELETE FROM user_permissions   WHERE borrowernumber = '187655';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='187661';
+DELETE FROM user_permissions   WHERE borrowernumber = '187661';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='187672';
+DELETE FROM user_permissions   WHERE borrowernumber = '187672';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='187676';
+DELETE FROM user_permissions   WHERE borrowernumber = '187676';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='196476';
+DELETE FROM user_permissions   WHERE borrowernumber = '196476';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='205901';
+DELETE FROM user_permissions   WHERE borrowernumber = '205901';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='212812';
+DELETE FROM user_permissions   WHERE borrowernumber = '212812';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='212930';
+DELETE FROM user_permissions   WHERE borrowernumber = '212930';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='213139';
+DELETE FROM user_permissions   WHERE borrowernumber = '213139';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='214054';
+DELETE FROM user_permissions   WHERE borrowernumber = '214054';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='214801';
+DELETE FROM user_permissions   WHERE borrowernumber = '214801';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='214868';
+DELETE FROM user_permissions   WHERE borrowernumber = '214868';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='219157';
+DELETE FROM user_permissions   WHERE borrowernumber = '219157';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='219176';
+DELETE FROM user_permissions   WHERE borrowernumber = '219176';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='219177';
+DELETE FROM user_permissions   WHERE borrowernumber = '219177';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='240971';
+DELETE FROM user_permissions   WHERE borrowernumber = '240971';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='258010';
+DELETE FROM user_permissions   WHERE borrowernumber = '258010';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='258107';
+DELETE FROM user_permissions   WHERE borrowernumber = '258107';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='258109';
+DELETE FROM user_permissions   WHERE borrowernumber = '258109';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='258145';
+DELETE FROM user_permissions   WHERE borrowernumber = '258145';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='258146';
+DELETE FROM user_permissions   WHERE borrowernumber = '258146';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='260910';
+DELETE FROM user_permissions   WHERE borrowernumber = '260910';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='261896';
+DELETE FROM user_permissions   WHERE borrowernumber = '261896';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='276197';
+DELETE FROM user_permissions   WHERE borrowernumber = '276197';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='276198';
+DELETE FROM user_permissions   WHERE borrowernumber = '276198';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='278051';
+DELETE FROM user_permissions   WHERE borrowernumber = '278051';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='280198';
+DELETE FROM user_permissions   WHERE borrowernumber = '280198';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='280199';
+DELETE FROM user_permissions   WHERE borrowernumber = '280199';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='286815';
+DELETE FROM user_permissions   WHERE borrowernumber = '286815';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='287134';
+DELETE FROM user_permissions   WHERE borrowernumber = '287134';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='290176';
+DELETE FROM user_permissions   WHERE borrowernumber = '290176';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='291312';
+DELETE FROM user_permissions   WHERE borrowernumber = '291312';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='291700';
+DELETE FROM user_permissions   WHERE borrowernumber = '291700';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='291753';
+DELETE FROM user_permissions   WHERE borrowernumber = '291753';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='291781';
+DELETE FROM user_permissions   WHERE borrowernumber = '291781';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='310215';
+DELETE FROM user_permissions   WHERE borrowernumber = '310215';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='321959';
+DELETE FROM user_permissions   WHERE borrowernumber = '321959';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='323818';
+DELETE FROM user_permissions   WHERE borrowernumber = '323818';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='325941';
+DELETE FROM user_permissions   WHERE borrowernumber = '325941';
+
+UPDATE borrowers SET flags='0' WHERE borrowernumber='329321';
+DELETE FROM user_permissions   WHERE borrowernumber = '329321';
+
