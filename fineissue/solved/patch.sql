98f5177f51785e01289dea1ce4951b6838e86ac8
Yashpal Meena <yaxhpal@gmail.com>
2014-02-18 12:13:15 +0530

    Updates

---
+++ fineissue/solved/patch.sql
@@ -0,0 +1,4 @@
+UPDATE items SET items.homebranch='PN' WHERE items.itemnumber='921744';
+UPDATE items SET items.homebranch='CH' WHERE items.itemnumber='1012352';
+UPDATE items SET items.homebranch='CH' WHERE items.itemnumber='97791';
+
