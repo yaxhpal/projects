759b79b5a3c2def2a6c7dd1b6434de51c544812b
Yashpal Meena <yaxhpal@gmail.com>
2014-02-23 00:48:45 +0530

    Update. closes ticket #1

--- staffmembers/restore-given-users.sql
+++ staffmembers/restore-given-users.sql
@@ -4,6 +4,6 @@ UPDATE borrowers SET flags='126686' WHERE borrowernumber='276197';
 UPDATE borrowers SET flags='261887' WHERE borrowernumber='41004';
 UPDATE borrowers SET flags='1572606' WHERE borrowernumber='49187'; /* Varinder */
 INSERT INTO user_permissions (borrowernumber,module_bit,code) VALUES (214054,13,'edit_news');
-
+UPDATE borrowers SET flags='1' WHERE borrowernumber='118864'
 
 
